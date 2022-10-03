be094a3fb653fc14d950a58fd4f5e26aa9959eb5
Zeddow <81985711+Zeddow@users.noreply.github.com>
2022-10-03 17:45:02 +0200

    Update crap price.js

--- crap price.js
+++ crap price.js
@@ -1,13 +1,53 @@
 const puppeteer = require('puppeteer');
 
-var hotel = [['welly', 'Kiel'],['crusoe', 'Bremen'],['dwarf', 'Berlin'],['hide', 'Berlin'],['flash', 'Berlin']];
+var hotel = [['welly', 'Kiel'],
+    ['crusoe', 'Bremen'],
+    ['dwarf', 'Berlin'],
+    ['hide', 'Berlin'],
+    ['flash', 'Berlin'],    
+    ['Amity', 'Potsdam'],
+    ['Hub', 'Düsseldorf'],
+    ['Form', 'Stuttgart'],
+    ['Fury', 'Aschheim'],
+    ['Air', 'Frankfurt am Main'],
+    ['Charly', 'Frankfurt am Main'],
+    ['Coin', 'Frankfurt am Main'],    
+    ['Fender', 'Amsterdam'],
+    ['Belt', 'Eschborn'],
+    ['Brass', 'München'],
+    ['Dairy', 'Haarlem'],
+    ['Loom', 'Manchester'],    
+    ['Franz', 'Wien'],
+    ['Hub', 'Düsseldorf'],
+    ['Form', 'Stuttgart'],
+    ['Loco', 'München'],
+    ['Mesh', 'Stuttgart'],
+    ['Rig', 'Lübeck'],
+    ['Mill', 'Köln'],    
+    ['Timber', 'Esslingen'],
+    ['Bricks', 'Hamburg'],
+    ['Mood', 'Mainz'],
+    ['Star', 'Sindelfingen'],    
+    ['Keg', 'Hamburg'],
+    ['Yen', 'Hamburg'],
+    ['Kettle', 'Stuttgart'],
+    ['Ridge', 'Halle'],
+    ['Cobbles', 'Essen'],
+    ['Cure', 'Erlangen'],
+    ['Square', 'Mannheim'],
+    ['Sparrow', 'Regensburg'],
+    ['Hop', 'Forchheim'],
+    ['Saddle', 'Fürth'],
+    ['Leo', 'Nürnberg'],
+    ['Tab', 'Düsseldorf'],
+    ['Seven', 'Düsseldorf']];
 var arrivalDate;
 var departureDate;
 var adults = 1;
 var rooms = 1;
 var link;
 var hotelPrices = [];
-var tageImVoraus = 1;
+var tageImVoraus = 2;
 var browser = '';
 
 
@@ -19,9 +59,10 @@ const prices = async () => {
         for (var i = 0; i < hotel.length; i++) {
             link = 'https://onepagebooking.com/niu' + hotel[i][0] + '?module=public&rooms=' + rooms + '&arrival=' + arrivalDate + '&departure=' + departureDate + '&adults=' + adults;
             var price = await getPrice(hotel[i][0], arrivalDate, link);
-            console.log("The NIU " + hotel[i][0].charAt(0).toUpperCase() + hotel[i][0].slice(1) + " in " + hotel[i][1] + " für " + price + "€ am " + arrivalDate + " gefunden.");
-            if (price < 100) {
-                
+            if (price == null) {
+                console.log("The NIU " + hotel[i][0].charAt(0).toUpperCase() + hotel[i][0].slice(1) + " in " + hotel[i][1] + " ist am " + arrivalDate + " nicht buchbar.");
+            }else if (price < 100) {
+                    console.log("The NIU " + hotel[i][0].charAt(0).toUpperCase() + hotel[i][0].slice(1) + " in " + hotel[i][1] + " für " + price + "€ am " + arrivalDate + " gefunden.");
                     hotelPrices.push([arrivalDate, "The NIU " + hotel[i][0].charAt(0).toUpperCase() + hotel[i][0].slice(1) + " in " + hotel[i][1], price])
                 }
         }
@@ -44,13 +85,20 @@ prices();
         const page = await browser.newPage();
         await page.goto(link);
         await page.waitForSelector('#submit-btn-wrap');
-        await page.
-            waitForSelector('.room-lowest-price')
-            .then(() => page.$eval('.room-lowest-price', el => el.textContent))
-            .then(text => {
-                 price = parseFloat(text.slice(3, -2));
+        try {
+            await page.
+                waitForSelector('.room-lowest-price')
+                .then(() => page.$eval('.room-lowest-price', el => el.textContent))
+                .then(text => {
+                    price = parseFloat(text.slice(3, -2));
                 
-            });
+                })
+        } catch (e)
+        {
+                await page.close();
+
+            return null;
+            };
         await page.close();
        
         return price;
